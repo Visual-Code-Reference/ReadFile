00ffedea2a7d5d36d89d98e07fd91aead342a265
John Sartain <jhnsartain3@gmail.com>
2022-03-25 22:20:15 -0500

    Add project files.

---
+++ ReadFile/Program.cs
@@ -0,0 +1,8 @@
+﻿var textFromFile = File.ReadAllText(@"C:\Users\John Sartain\Documents\visualcodereference\How to read a file with C#\test.txt");
+
+Console.WriteLine(textFromFile);
+
+var allLinesFromFile = File.ReadAllLines(@"C:\Users\John Sartain\Documents\visualcodereference\How to read a file with C#\test.txt");
+
+foreach (var line in allLinesFromFile)
+    Console.WriteLine(line);
